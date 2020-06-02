44672f339f3fa843ca537bd9837f261ede438c2f
drocca <drocca@mitre.org>
2020-06-02 10:22:01 -0400

    Update runserver to ignore certs

--- run-server.sh
+++ run-server.sh
@@ -4,4 +4,4 @@ VONQBEPORT=${1:-8080}
 echo "Spring port: $VONQBEPORT"
 export VONQBEPORT
 
-mvn spring-boot:run
+mvn spring-boot:run -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
